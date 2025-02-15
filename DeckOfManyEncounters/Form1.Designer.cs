98ff8e786ea03b3cb1e7dfdecb5e752975c68979
Zachary Adelson <zachary.adelson@wsu.edu>
2025-02-15 14:47:49 -0800

    idk

--- DeckOfManyEncounters/Form1.Designer.cs
+++ DeckOfManyEncounters/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
             this.ClientSize = new System.Drawing.Size(1002, 712);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
