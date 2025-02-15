d6edefa9934c91dde3c31e97f5c02cccd480d242
willowzuvich <zuvichwil@gmail.com>
2025-02-15 14:58:36 -0800

    Player class

--- DeckOfManyEncounters/Monster.cs
+++ DeckOfManyEncounters/Monster.cs
@@ -12,83 +12,83 @@ namespace DeckOfManyEncounters
     }
     internal class Monster
     {
-        private string name;
+        private string name = string.Empty;
 
         public string Name { get { return name; } set { name = value; } }
 
-        private Size size;
+        private Size size = Size.Small;
 
         public Size Size { get { return size; } set { size = value; } }   
 
-        private string type;
+        private string type = string.Empty;
 
         public string Type { get { return type; } set { type = value; } } 
 
-        private string alignment;
+        private string alignment = string.Empty;
 
         public string Alignment { get { return alignment; } set { alignment = value; } }   
 
-        private int ac;
+        private int ac = 0;
 
         public int AC { get { return ac; } set { ac = value; } }
 
-        private int hp;
+        private int hp = 0;
 
         public int HP { get { return hp; } set { hp = value;  } }
 
-        private string speed;
+        private string speed = string.Empty;
 
         public string Speed { get { return speed; } set { speed = value; } }
 
-        private int str;
+        private int str = 0;
 
         public int STR { get { return str; } set { str = value; } }
 
-        private int dex;
+        private int dex = 0;
 
         public int DEX { get { return dex;  } set { dex = value; } }
 
-        private int con; 
+        private int con = 0; 
 
         public int CON { get { return con; } set { con = value; } }
 
-        private int intelligence;
+        private int intelligence = 0;
 
         public int Intelligence { get { return intelligence; } set { intelligence = value; } }
 
-        private int wis;
+        private int wis = 0;
 
         public int WIS { get { return wis; } set { wis = value; } };
 
-        private int cha;
+        private int cha = 0;
 
         public int CHA { get { return cha; } set { cha = value; } }
 
-        private string savThrows;
+        private string savThrows = string.Empty;
 
         public string SavThrows { get { return savThrows; } set { savThrows = value; } }
 
-        private string skills;
+        private string skills = string.Empty;
 
         public string Skills { get { return skills; } set { skills = value; } } 
 
-        private string wri;
+        private string wri = string.Empty;
 
         public string WRI { get { return wri; } set { wri = value; } }
 
-        private string senses;
+        private string senses = string.Empty;
 
         public string Senses { get { return senses; } set { senses = value; } }
 
-        private string languages;
+        private string languages = string.Empty;
 
         public string Languages { get { return languages; } set { languages = value; } }    
 
-        private double challengeRating;
+        private double challengeRating = 0.0;
 
         public double ChallengeRating { get { return ChallengeRating; } set { ChallengeRating = value; } }  
 
-        private string additional; 
+        private string additional = string.Empty; 
 
         public string Additional { get { return additional; } set { additional = value; } }
 
---
+++ DeckOfManyEncounters/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfManyEncounters
+{
+    internal class Player
+    {
+        private int level = 0; 
+
+        public int Level { get { return level; } set { level = value; } }  
+
+        private string name = string.Empty;
+
+        public string Name { get { return name; } set { name = value; } }   
+
+        private string classification = string.Empty; 
+
+        public string Classification { get { return classification; } set { classification = value; } } 
+
+
+    }
+}
