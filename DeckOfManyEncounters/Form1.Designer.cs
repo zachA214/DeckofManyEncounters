64c1282abc4b1ca6acb6ad66974402d1b4acdce4
willowzuvich <zuvichwil@gmail.com>
2025-02-16 02:24:52 -0800

    Added artistic flair to form1

--- DeckOfManyEncounters/Form1.Designer.cs
+++ DeckOfManyEncounters/Form1.Designer.cs
@@ -36,9 +36,11 @@
             // 
             // inputFormOpenButton
             // 
-            this.inputFormOpenButton.Location = new System.Drawing.Point(687, 294);
+            this.inputFormOpenButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputFormOpenButton.Location = new System.Drawing.Point(912, 355);
+            this.inputFormOpenButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.inputFormOpenButton.Name = "inputFormOpenButton";
-            this.inputFormOpenButton.Size = new System.Drawing.Size(157, 44);
+            this.inputFormOpenButton.Size = new System.Drawing.Size(210, 55);
             this.inputFormOpenButton.TabIndex = 0;
             this.inputFormOpenButton.Text = "Encounter Input";
             this.inputFormOpenButton.UseVisualStyleBackColor = true;
@@ -46,9 +48,12 @@
             // 
             // partyButton
             // 
-            this.partyButton.Location = new System.Drawing.Point(189, 294);
+            this.partyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.partyButton.ForeColor = System.Drawing.Color.Black;
+            this.partyButton.Location = new System.Drawing.Point(252, 355);
+            this.partyButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.partyButton.Name = "partyButton";
-            this.partyButton.Size = new System.Drawing.Size(130, 34);
+            this.partyButton.Size = new System.Drawing.Size(210, 55);
             this.partyButton.TabIndex = 1;
             this.partyButton.Text = "View Party";
             this.partyButton.UseVisualStyleBackColor = true;
@@ -56,9 +61,11 @@
             // 
             // encounterGenerationButton
             // 
-            this.encounterGenerationButton.Location = new System.Drawing.Point(401, 462);
+            this.encounterGenerationButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.encounterGenerationButton.Location = new System.Drawing.Point(535, 578);
+            this.encounterGenerationButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.encounterGenerationButton.Name = "encounterGenerationButton";
-            this.encounterGenerationButton.Size = new System.Drawing.Size(210, 66);
+            this.encounterGenerationButton.Size = new System.Drawing.Size(280, 82);
             this.encounterGenerationButton.TabIndex = 2;
             this.encounterGenerationButton.Text = "Generate Encounters";
             this.encounterGenerationButton.UseVisualStyleBackColor = true;
@@ -66,19 +73,20 @@
             // 
             // DeckOfManyEncounters
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Desktop;
-            this.ClientSize = new System.Drawing.Size(1002, 712);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(1331, 871);
             this.Controls.Add(this.encounterGenerationButton);
             this.Controls.Add(this.partyButton);
             this.Controls.Add(this.inputFormOpenButton);
             this.Cursor = System.Windows.Forms.Cursors.Arrow;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(1024, 768);
-            this.MinimumSize = new System.Drawing.Size(1024, 768);
+            this.MaximumSize = new System.Drawing.Size(1357, 942);
+            this.MinimumSize = new System.Drawing.Size(1357, 942);
             this.Name = "DeckOfManyEncounters";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Deck of Many Encounters";
