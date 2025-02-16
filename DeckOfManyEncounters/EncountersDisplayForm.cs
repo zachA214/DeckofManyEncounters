3e7827747adcb7e894d44b72cde46c4ae9dfaa11
Zachary Adelson <zachary.adelson@wsu.edu>
2025-02-16 03:27:59 -0800

    encounter 1 finalized

--- DeckOfManyEncounters/EncountersDisplayForm.cs
+++ DeckOfManyEncounters/EncountersDisplayForm.cs
@@ -14,6 +14,11 @@ namespace DeckOfManyEncounters
     public partial class EncountersDisplayForm: Form
     {
         private int yPos = 10;
+        private Image small = Image.FromFile("MonsterImages/TinyFly.png");
+        private Image medium = Image.FromFile("MonsterImages/medium_guy.png");
+        private Image large = Image.FromFile("MonsterImages/OrcLarge.png");
+        private Image huge = Image.FromFile("MonsterImages/huge_guy.png");
+        private Image garg = Image.FromFile("MonsterImages/PurpleWormGarg.png");
         internal EncountersDisplayForm(List<Encounter> encounterList)
         {
             InitializeComponent();
@@ -27,7 +32,30 @@ namespace DeckOfManyEncounters
                 monsterName.Location = new Point(30, yPos); // Position of delete button
                 //monsterName.Click += (sender, e) => DeleteEntry(panel, deleteButton, CM1);
 
+                PictureBox monsterImage = new PictureBox();
+                switch (encounterList[0].MonsterList[i].Size)
+                {
+                    case "Small":
+                        monsterImage.Image = small;
+                        break;
+                    case "Medium":
+                        monsterImage.Image = medium;
+                        break;
+                    case "Large":
+                        monsterImage.Image = large;
+                        break;
+                    case "Huge":
+                        monsterImage.Image = huge;
+                        break;
+                    case "Gargantuan":
+                        monsterImage.Image = garg;
+                        break;
+                }
+                monsterImage.Location = new Point(280, yPos);
+                monsterImage.Size = new Size(60, 60);
+
                 panel1.Controls.Add(monsterName);
+                panel1.Controls.Add(monsterImage);
 
 
                 yPos += 40; // Adjust this value for spacing between entries
@@ -36,3 +64,4 @@ namespace DeckOfManyEncounters
         }
     }
 }
+
